cd66ad00ac6daa8557809f4fa95fdef7f2d4335d
Nurseit <bncer.ml@gmail.com>
2025-10-07 01:40:14 +0600

    Add .env

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ extern crate hyper;
 extern crate hyper_rustls;
 use rustls::crypto::CryptoProvider;
 use sheets4::{Result, Sheets, yup_oauth2};
+use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    CryptoProvider::install_default(rustls::crypto::ring::default_provider())
+    dotenv().ok();
+
+    CryptoProvider::install_default(rustls::crypto::aws_lc_rs::default_provider())
         .expect("Failed to install default crypto provider");
-    // 1. Path to your service account key file
-    let sa_key_path = "./credentials.json";
+
+    let sa_key_path = env::var("GOOGLE_CREDENTIALS").expect("Json file with Google credentials must be in .env file");
     let sa_key = yup_oauth2::read_service_account_key(sa_key_path)
         .await
         .expect("Couldn't read service account key");
 
-    // 2. Create an authenticator
     let auth = yup_oauth2::ServiceAccountAuthenticator::builder(sa_key)
         .build()
         .await
@@ -30,21 +33,17 @@ async fn main() -> Result<()> {
                 .build(),
         );
 
-    // 4. Create the Sheets Hub
     let hub = Sheets::new(client, auth);
 
-    // 5. Define your spreadsheet ID and the range to read
-    let spreadsheet_id = "1diOg_IPD11a36e9PiQOP-pTOdNM-qMowrhkKPR-z8pQ";
+    let spreadsheet_id = env::var("GOOGLE_SHEET_ID").expect("Google Sheets ID must be in .env file");
     let range = "Sheet1!A1:C10";
 
-    // 6. Make the API call to get values
     let result = hub
         .spreadsheets()
-        .values_get(spreadsheet_id, range)
+        .values_get(&spreadsheet_id, range)
         .doit()
         .await;
 
-    // 7. Handle the response
     match result {
         Ok((_response, value_range)) => {
             if let Some(values) = value_range.values {
